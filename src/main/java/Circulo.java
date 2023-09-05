50d10d64b0812e03c8d6332daef90e0d4edd8e50
Eduarda Souto <mariaeduardac604@gmail.com>
2023-09-05 08:40:00 -0300

    Terminando circulo

--- src/main/java/Circulo.java
+++ src/main/java/Circulo.java
@@ -1,5 +1,25 @@
-public class Circulo{
-    float raio;
-    float area;
-    float circunferencia;
+public class Circulo {
+    private float raio;
+    public float area;
+    public float circunferencia;
+
+    public Circulo(float raio) {
+        this.raio = raio;
+    }
+
+    public float getRaio() {
+        return raio;
+    }
+
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+
+    public float getArea() {
+        return (float) (3.14159 * (raio * raio));
+    }
+
+    public float getCircunferencia() {
+        return (float) (2 * 3.14159 * raio);
+    }
 }
